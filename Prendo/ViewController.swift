c8c15170f2d58826603d42695351aa4726c70dcf
Opeoluwa Williams <davwilliams6@gmail.com>
2020-05-29 20:49:09 -0700

    Initial Commit

---
+++ Prendo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prendo
+//
+//  Created by user169361 on 5/29/20.
+//  Copyright © 2020 The Prendos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
